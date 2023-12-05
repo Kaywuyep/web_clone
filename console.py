87bdd98976472722fb5cba71d74b48dc6253f72e
Kaywuyep <wuyepkate@gmail.com>
2023-12-05 06:15:18 -0800

    task

--- console.py
+++ console.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """contains the entry point of the command interpreter"""
 import cmd
+import shlex  # for splitting strings into token, esp in parsing cmd-line input
 from models.base_model import BaseModel
-from models import storage
+from models.__init__ import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,6 +11,9 @@ class HBNBCommand(cmd.Cmd):
     my command interpreter
     """
     prompt = "(hbnb) "
+    list_objClass = [
+            "BaseModel"
+            ]
 
     def do_quit(self, arg):
         """ Quit command to exit the program\n"""
@@ -23,6 +27,48 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """do not execute anything"""
         pass
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel,
+        saves it to the JSON file, and prints the id.
+        Ex: $ create BaseModel
+        """
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.list_objClass:
+            print("** class doesn't exist **")
+        else:
+            new_instance = globals()[args[0]]()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+         Prints the string representation of an instance
+         based on the class name and id. Ex: $ show
+        """
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.list_objClass:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            """
+            We have to check if the 'id' exists, to achieve that we need to
+            create id_obj with the form Classname.id that is the key that
+            we will use to ask if it is in Storage and
+            can retrieve the value for that key, if the id_obj is not in
+            storage then print 'no instance found'
+            """
+            id_obj = "{}.{}".format(args[0], args[1])
+            if id_obj in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
